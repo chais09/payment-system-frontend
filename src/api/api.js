d5d044aeffdac33a88ed1139b9dfe053c2e1c470
Chai Seng Loi <chaiseng0923@gmail.com>
2025-12-25 21:14:29 +0800

    added free tier exhausted message

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL || "https://wise-payments-microservice-lu8r.onrender.com",
+    baseURL: "https://wise-payments-microservice.onrender.com",
 });
 
 // Add a response interceptor
